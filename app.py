62d0504670b8b19f92c7b20a0f116e07a24a162b
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2023-12-03 19:24:55 +0530

    printing out the transcript to frontend

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    transcript = ""
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
 
-    if (request.method == 'POST'):
-        print(request.files)
+        if "file" not in request.files:
+            return redirect(request.url)
 
-    if "file" not in request.files:
-        return redirect(request.url)
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
 
-    file = request.files["file"]
-    if (file.filename == ""):
-        return redirect(request.url)
-
-    if file:
-        recognizer = sr.Recognizer()
-        audioFile = sr.AudioFile(file)
-        with audioFile as source:
-            data = recognizer.record(source)
-        text = recognizer.recognize_google_cloud(data, key=none)
-
-    return render_template('index.html')
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None)
+            print(transcript)
+    return render_template('index.html', transcript=transcript)
 
 
 if __name__ == '__main__':
